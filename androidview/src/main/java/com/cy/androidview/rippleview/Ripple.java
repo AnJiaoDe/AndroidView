891c55a5469c95f6e8de5f8595240baeff15ad7a
437042721@qq.com <437042721@qq.com>
2020-10-02 22:35:07 +0800

    修改水波纹

--- androidview/src/main/java/com/cy/androidview/rippleview/Ripple.java
+++ androidview/src/main/java/com/cy/androidview/rippleview/Ripple.java
@@ -71,10 +71,23 @@ public class Ripple {
                 drawable_bg.setAlpha(0);
                 drawable_bg.setBounds(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
 
-                drawable_mask = new GradientDrawable();
-                drawable_mask.setColor(0xffffffff);
-                drawable_mask.setCornerRadius(view.getWidth() * 1f / 2);
-                drawable_mask.setStroke(1, 0xffffffff);
+//                drawable_mask = new GradientDrawable();
+//                drawable_mask.setColor(0xffffffff);
+//                if(view.getWidth()>view.getHeight()){
+//                    drawable_mask.setBounds((int) (view.getLeft()+view.getWidth()*1f/2-view.getHeight()*1f/2),
+//                            view.getTop(),
+//                            (int) (view.getRight()-view.getWidth()*1f/2+view.getHeight()*1f/2),
+//                            view.getBottom());
+//                    drawable_mask.setGradientRadius(10);
+//                    drawable_mask.setCornerRadius(view.getHeight() * 1f / 2);
+//                }else {
+//                    drawable_mask.setBounds(view.getLeft(),
+//                            (int) (view.getTop()+view.getHeight()*1f/2-view.getWidth()*1f/2),
+//                            view.getRight(),
+//                            (int) (view.getBottom()-view.getHeight()*1f/2+view.getWidth()*1f/2));
+//                    drawable_mask.setCornerRadius(view.getWidth() * 1f / 2);
+//                }
+//                drawable_mask.setStroke(1, 0xffffffff);
             }
             //当控件设置了点击监听器，并且控件点击有效，时，才能产生水波纹
             rippleDrawable = new RippleDrawable(ColorStateList.valueOf(colorRipple), drawable_bg, drawable_mask);
