b7bb727a424f4589de2e086b5682977cbd1aabbf
437042721@qq.com <437042721@qq.com>
2020-11-06 17:32:41 +0800

    updated

--- androidview/src/main/java/com/cy/androidview/rippleview/Ripple.java
+++ androidview/src/main/java/com/cy/androidview/rippleview/Ripple.java
@@ -62,38 +62,38 @@ public class Ripple {
 
     public Ripple ripple() {
         //5.0以上才有效,
-        if (havaRipple && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            RippleDrawable rippleDrawable;
-            Drawable drawable_bg = view.getBackground();
-            GradientDrawable drawable_mask = null;
-            if (drawable_bg == null) {
-                drawable_bg = new ShapeDrawable();
-                drawable_bg.setAlpha(0);
-                drawable_bg.setBounds(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
-
-//                drawable_mask = new GradientDrawable();
-//                drawable_mask.setColor(0xffffffff);
-//                if(view.getWidth()>view.getHeight()){
-//                    drawable_mask.setBounds((int) (view.getLeft()+view.getWidth()*1f/2-view.getHeight()*1f/2),
-//                            view.getTop(),
-//                            (int) (view.getRight()-view.getWidth()*1f/2+view.getHeight()*1f/2),
-//                            view.getBottom());
-//                    drawable_mask.setGradientRadius(10);
-//                    drawable_mask.setCornerRadius(view.getHeight() * 1f / 2);
-//                }else {
-//                    drawable_mask.setBounds(view.getLeft(),
-//                            (int) (view.getTop()+view.getHeight()*1f/2-view.getWidth()*1f/2),
-//                            view.getRight(),
-//                            (int) (view.getBottom()-view.getHeight()*1f/2+view.getWidth()*1f/2));
-//                    drawable_mask.setCornerRadius(view.getWidth() * 1f / 2);
-//                }
-//                drawable_mask.setStroke(1, 0xffffffff);
-            }
-            //当控件设置了点击监听器，并且控件点击有效，时，才能产生水波纹
-            rippleDrawable = new RippleDrawable(ColorStateList.valueOf(colorRipple), drawable_bg, drawable_mask);
-
-            view.setBackground(rippleDrawable);
-        }
+//        if (havaRipple && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+//            RippleDrawable rippleDrawable;
+//            Drawable drawable_bg = view.getBackground();
+//            GradientDrawable drawable_mask = null;
+//            if (drawable_bg == null) {
+//                drawable_bg = new ShapeDrawable();
+//                drawable_bg.setAlpha(0);
+//                drawable_bg.setBounds(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+//
+////                drawable_mask = new GradientDrawable();
+////                drawable_mask.setColor(0xffffffff);
+////                if(view.getWidth()>view.getHeight()){
+////                    drawable_mask.setBounds((int) (view.getLeft()+view.getWidth()*1f/2-view.getHeight()*1f/2),
+////                            view.getTop(),
+////                            (int) (view.getRight()-view.getWidth()*1f/2+view.getHeight()*1f/2),
+////                            view.getBottom());
+////                    drawable_mask.setGradientRadius(10);
+////                    drawable_mask.setCornerRadius(view.getHeight() * 1f / 2);
+////                }else {
+////                    drawable_mask.setBounds(view.getLeft(),
+////                            (int) (view.getTop()+view.getHeight()*1f/2-view.getWidth()*1f/2),
+////                            view.getRight(),
+////                            (int) (view.getBottom()-view.getHeight()*1f/2+view.getWidth()*1f/2));
+////                    drawable_mask.setCornerRadius(view.getWidth() * 1f / 2);
+////                }
+////                drawable_mask.setStroke(1, 0xffffffff);
+//            }
+//            //当控件设置了点击监听器，并且控件点击有效，时，才能产生水波纹
+//            rippleDrawable = new RippleDrawable(ColorStateList.valueOf(colorRipple), drawable_bg, drawable_mask);
+//
+//            view.setBackground(rippleDrawable);
+//        }
         return this;
     }
 
