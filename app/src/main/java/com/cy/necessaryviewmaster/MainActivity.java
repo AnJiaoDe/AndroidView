d00cc7f71d085da7b2970000d71a2b77ad4571aa
CY <437042721@qq.com>
2024-09-04 16:57:14 +0800

    update

--- app/src/main/java/com/cy/necessaryviewmaster/MainActivity.java
+++ app/src/main/java/com/cy/necessaryviewmaster/MainActivity.java
@@ -1,7 +1,15 @@
 package com.cy.necessaryviewmaster;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends BaseActivity {
@@ -24,6 +32,42 @@ public class MainActivity extends BaseActivity {
         for(String s:str.split("")){
             LogUtils.log("String",s);
         }
+
+        /**
+         * flag 参数的作用
+         * flag 参数是一个整数值，指定如何处理新添加的文本的样式和格式。常见的标志值包括：
+         * Spannable.SPAN_EXCLUSIVE_EXCLUSIVE: 新文本的样式不包括在原文本中（即，样式只应用于新文本）。
+         * Spannable.SPAN_EXCLUSIVE_INCLUSIVE: 新文本的样式从其开始位置开始，但不包括原文本的结束位置。
+         * Spannable.SPAN_INCLUSIVE_EXCLUSIVE: 新文本的样式从原文本的起始位置开始，但不包括新文本的结束位置。
+         * Spannable.SPAN_INCLUSIVE_INCLUSIVE: 新文本的样式包括原文本的起始和结束位置。
+         */
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append("Hello, ");
+        builder.append("colorful", new ForegroundColorSpan(Color.RED), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(" and ");
+        builder.append("size varied", new RelativeSizeSpan(2f), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(" text!");
+
+        TextView tv=findViewById(R.id.tv);
+        tv.setText(builder);
+
+
+        // 添加文本
+        builder.append("Hello, ");
+        int start = builder.length();
+        builder.append("world!");
+        int end = builder.length();
+
+        // 设置粗体
+        builder.setSpan(new StyleSpan(Typeface.NORMAL), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 设置斜体
+        builder.setSpan(new StyleSpan(Typeface.ITALIC), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 设置文本颜色
+        builder.setSpan(new ForegroundColorSpan(Color.RED), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 设置文本相对大小
+        builder.setSpan(new RelativeSizeSpan(1.5f), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // 设置 SpannableStringBuilder 为 TextView 的内容
+        tv.setText(builder);
     }
 
     @Override
